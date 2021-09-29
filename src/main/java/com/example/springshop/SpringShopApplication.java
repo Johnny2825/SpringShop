4cfc8be32cb8a2741dd3d5adcd19c0577fdde688
Johnny2825 <35284499+Johnny2825@users.noreply.github.com>
2021-09-29 17:48:08 +1000

    init

---
+++ src/main/java/com/example/springshop/SpringShopApplication.java
@@ -0,0 +1,28 @@
+package com.example.springshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringShopApplication.class, args);
+    }
+
+}
+
+/**
+ * 1. Дописать скрипты в liquibase
+ * 2. Создать сущности под таблицы
+ */
+
+
+/**
+ * Товар - наименования, цена, количество, автор, артикул, категория, дата создания
+ * Пользователь - Имя, Фамилия, Отчество, Телефон, Адрес, Баланс, Роль, Почта, дата создания, логин, пароль
+ * Заказ - Корзина, Стоимость заказа, Пользователь, Номер заказа, Адрес доставки,
+ * дата создания(Дополнительно расчет стоимости заказа в зависимости от веса) стоиимости
+ * Категория - Наименование, дата создания
+ * Корзина - дата создания, список товаров и их количество, создатель(Пользователь)
+ */
